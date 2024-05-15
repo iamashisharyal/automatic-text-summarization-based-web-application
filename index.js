c198974bda9ad996e02151ea89ad7b7abb0cced4
iamashisharyal <138591030+iamashisharyal@users.noreply.github.com>
2024-05-15 13:22:58 +0545

    Delete index.js

--- index.js
+++
@@ -1,8 +0,0 @@
-var url = "https://sheets.googleapis.com/v4/spreadsheets/1Ih2hXsFmos6PI9LJNhzR1lFpqTd14GCsCM4MvrEiCpk/values/Sheet1!A1:Z?alt=json&key=AIzaSyB_hBOuHVGTCCY-ktx8om-7ZLdCLo2vlaM";                                                             
-axios.get(url)
-  .then(function (response) {
-    console.log(response);                                                                                                                                                    
-  })
-  .catch(function (error) {
-    console.log(error);                                                                                                                                                       
-  });                
